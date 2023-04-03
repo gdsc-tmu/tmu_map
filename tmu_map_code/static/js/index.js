31829c783c90edf17c7caf82b8eb66d1c8a1d96c
kisu-yuyu <kisu-yujiro@ed.tmu.ac.jp>
2023-04-04 00:24:42 +1000

    Adding a simple Google Map

---
+++ tmu_map_code/static/js/index.js
@@ -0,0 +1,12 @@
+var map;
+function initMap() {
+  var latlng = { lat: 35.61712271168633, lng: 139.3780743025393 };
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: latlng,
+    zoom: 18,
+  });
+  const ctaLayer = new google.maps.KmlLayer({
+    url: "",
+    
+  });
+}
